cf2dc73b26bcf238aed13abaae93ad4ffeb0e3e5
Aman Raj <amanraj68bittu@gmail.com>
2022-06-29 22:55:54 +0530

    Create morseCode.ino

---
+++ morseCode.ino
@@ -0,0 +1,46 @@
+int led = 4;
+int buzzer = 6;
+
+void setup(){
+    pinMode(led, OUTPUT);
+    pinMode(buzzer, OUTPUT);
+    w();
+    lspace();
+    e();
+    lspace();
+    l();
+    lspace();
+    c();
+    lspace();
+    o();
+    lspace();
+    m();
+    lspace();
+    e();
+    wspace();
+    t();
+    lspace()
+    o();
+    wspace();
+    m();
+    lspace();
+    y();
+    wspace();
+    c();
+    lspace();
+    h();
+    lspace();
+    a();
+    lspace();
+    n();
+    lspace();
+    n();
+    lspace();
+    e();
+    lspace();
+    l();
+    wspace();
+
+}
+
+void()
