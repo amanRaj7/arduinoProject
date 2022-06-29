4ab9ba133d7e4f339aac80df8aabe9379c038c36
Aman Raj <amanraj68bittu@gmail.com>
2022-06-29 23:36:44 +0530

    Update morseCode.ino
    Morse Code (Welcome to my channel)

--- morseCode.ino
+++ morseCode.ino
@@ -1,6 +1,178 @@
 int led = 4;
 int buzzer = 6;
 
+void w(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    
+}
+
+void e(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+}
+
+void l(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+}
+
+void c(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+}
+
+void o(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+}
+
+void m(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+}
+
+
+void y(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+}
+
+void t(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3); 
+}
+
+void h(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+}
+
+void a(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+}
+
+void n(){
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(3);
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(1);
+}
+
+void lspace(){
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    delay(3);
+}
+
+void wspace(){
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    delay(7);
+}
 void setup(){
     pinMode(led, OUTPUT);
     pinMode(buzzer, OUTPUT);
@@ -42,5 +214,3 @@ void setup(){
     wspace();
 
 }
-
-void()
